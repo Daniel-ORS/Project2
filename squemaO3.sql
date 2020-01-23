c4fa3a70bd277a1e8c9848b5c8d61632bb911fb8
DANIEL ORTIZ-RUBIO <dorsmx@hotmail.com>
2020-01-22 22:10:53 -0600

    “Tarea”

---
+++ squemaO3.sql
@@ -0,0 +1,22 @@
+CREATE TABLE O3 (
+  _id_x varchar ,
+  lat dec ,
+  long dec,
+  id_x int,
+  nombre varchar,
+  codigo varchar,
+  redesid int,
+  _id_y varchar,
+  city varchar,
+  date date,
+  estacionesid int,
+  fecha date,
+  hora int,
+  id_y varchar,
+  parametro varchar,
+  state varchar,
+  validoorig int,
+  valororig dec
+);
+
+													
